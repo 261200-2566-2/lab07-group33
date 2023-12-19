db294cc6196d335c288da8d36b70bee3ce839096
doraemon <doraemonprogramming@gmail.com>
2023-12-19 19:15:42 +0700

    Done?

--- src/Lab07.java
+++ src/Lab07.java
@@ -1,26 +1,48 @@
-public class Lab07{
+import java.util.Arrays;
 
-    public static void main(String[] args){
-        Set<String> set = new Set<>();
 
-        set.add("a");
-        set.add("b");
-        set.add("c");
 
-        System.out.println("Set Size: " + set.size());
-        System.out.println("Contain a?: " + set.contains("a"));
-        System.out.println("Contain b?: " + set.contains("b"));
-        System.out.println("Contain c?: " + set.contains("c"));
-        System.out.println("Contain hello?: " + set.contains("hello"));
+class Main {
+    public static void main(String[] args) {
 
-        set.remove("a");
+        Set<String> colors = new Set<>();
 
-        System.out.println("Contain \"a\" after removal?: " + set.contains("a"));
 
-        set.clear();
+        System.out.println("Is set empty: " + colors.isEmpty());
 
-        System.out.println("Set size after cleared: " + set.size());
-        System.out.println("is Set empty?: " + set.isEmpty());
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+
+        System.out.println("Set size after adding elements: " + colors.size());
+
+        System.out.println("Set contains 'Red': " + colors.contains("Red"));
+
+        colors.remove("Blue");
+
+        System.out.println("Set size after removing element: " + colors.size());
+
+        System.out.println("Colors set contains all selected colors: " + colors.containsAll(Arrays.asList("Red", "Green")));
+
+        colors.removeAll(Arrays.asList("Red", "Green"));
+
+        System.out.println("Colors set after removing elements: " + colors.size());
+
+        colors.addAll(Arrays.asList("Yellow", "Purple", "Orange"));
+
+        System.out.println("Colors set after adding several elements at once: " + colors.size());
+
+        colors.retainAll(Arrays.asList("Yellow","Purple"));
+
+        System.out.println("Set size after retaining: " + colors.size());
+
+
+        colors.clear();
+
+        System.out.println("Set empty after clearing: " + colors.isEmpty());
     }
-
 }
+
+
+
+
